daa07398364c989779cbde9dcb88814817544924
ink <2364362075@qq.com>
2025-10-22 17:09:42 +0800

    训练参数调整

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ from model import MultiFrameCTDenoiser
 import train_dataset
 import math
 from matplotlib import pyplot
+import wandb
+wandb.init(project="newCT")
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--dataset_path', type=str, default='dataset\L333')
 arg_parser.add_argument('--checkpoint_path', type=str, default='checkpoint')
@@ -13,12 +15,12 @@ arg_parser.add_argument('--n_epochs', type=int, default=200)
 arg_parser.add_argument('--batch_size', type=int, default=1)
 arg_parser.add_argument('--threads', type=int, default=0)
 arg_parser.add_argument("--warmup", type=int, default=100)
-arg_parser.add_argument("--init_lr", type=float, default=2e-4)
-arg_parser.add_argument("--final_lr", type=float, default=2e-4)
+arg_parser.add_argument("--init_lr", type=float, default=1e-6)
+arg_parser.add_argument("--final_lr", type=float, default=1e-6)
 arg_parser.add_argument("--cos_lr", type=bool, default=True)
-arg_parser.add_argument('--b1', type=float, default=0.5)
+arg_parser.add_argument('--b1', type=float, default=0.99)
 arg_parser.add_argument('--b2', type=float, default=0.999)
-arg_parser.add_argument('--clip_grad', type=float, default=2.0,  
+arg_parser.add_argument('--clip_grad', type=float, default=0,  
                         help='梯度裁剪阈值，设为0或None表示不裁剪')
 args = arg_parser.parse_args()
 print(args)
@@ -92,6 +94,6 @@ for epoch in range(args.n_epochs):
                                                                                    len(train_dataset),
                                                                                    loss / 2,
                                                                                    pixel_loss))
-
+        wandb.log({"loss": loss.item()/2, "pixel_loss": pixel_loss.item(), "lr": lr})
 
     torch.save(model.state_dict(), os.path.join(args.checkpoint_path, 'inknet%d.pth' % epoch))
